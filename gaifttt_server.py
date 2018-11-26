1e04a623a4a3be30de59eb8c6544474d21e6a872
Gabriel <gabriel.zima@gmail.com>
2018-11-26 10:16:06 +0100

    Update gaifttt_server.py

--- gaifttt_server.py
+++ gaifttt_server.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/say/?text=<phrase>")
+def say():
+    return "Say
     
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=80) 
+app.run(host='0.0.0.0', port=8082) 

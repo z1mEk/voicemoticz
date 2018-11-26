257f4d6f9af0651582234c3f73165de5c3347257
Gabriel <gabriel.zima@gmail.com>
2018-11-26 10:26:23 +0100

    Update gaifttt_server.py

--- gaifttt_server.py
+++ gaifttt_server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def say():
     text = request.args.get("text")
     return "Hello World!"
+
+@app.route("/test/<filename>")
+def test(filename):
+    return filename
     
 if __name__ == '__main__':
 app.run(host='0.0.0.0', port=8082) 

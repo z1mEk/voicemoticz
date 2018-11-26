a4d5a002c6641f772a3571f81c2174e071d173f9
Gabriel <gabriel.zima@gmail.com>
2018-11-26 10:38:04 +0100

    Update gaifttt_server.py

--- gaifttt_server.py
+++ gaifttt_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ def say():
 @app.route("/test/<filename>")
 def test(filename):
     return filename
+
+@app.route('/foo', methods=['POST']) 
+def foo():
+    if not request.json:
+        abort(400)
+    print request.json
+    return json.dumps(request.json)
     
 if __name__ == '__main__':
 app.run(host='0.0.0.0', port=8082) 
